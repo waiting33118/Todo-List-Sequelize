fa58ac2abbfe2faa098f3d5e2a6809ec99f1cef8
Tony <waiting33118@gmail.com>
2020-06-27 21:18:12 +0800

    feat: create routes

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const home = require('./modules/home')
+const users = require('./modules/users')
+const todos = require('./modules/todos')
+const { authenticator } = require('../middleware/auth')
+
+router.use('/users', users)
+router.use('/todos', authenticator, todos)
+router.use('/', authenticator, home)
+
+module.exports = router
